38ea72679a3e6aa707bc17f35958a5c059f3a065
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-08-04 17:17:49 -0500

    Create PR6
    practice PR6 with rabbitMQ

--- backend/users_service/users_service.py
+++ backend/users_service/users_service.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# Middleware CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -19,7 +20,7 @@ app.add_middleware(
 rabbit_host = os.getenv("RABBITMQ_HOST", "rabbitmq")
 
 def get_connection():
-    for attempt in range(10):  # hasta 10 intentos
+    for attempt in range(10):  # Up to 10 tries
         try:
             print(f"🔧 Intento {attempt+1}/10: conectando a RabbitMQ en {rabbit_host}...")
             connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host))
@@ -30,10 +31,13 @@ def get_connection():
             time.sleep(5)
     raise Exception("No se pudo conectar a RabbitMQ después de 10 intentos")
 
-# Establecer conexión y canal global
+# Conection with RabbitMQ
 connection = get_connection()
 channel = connection.channel()
 channel.queue_declare(queue="notifications")
+
+# Temporal list memory
+usuarios_registrados = []
 
 class User(BaseModel):
     nombre: str
@@ -41,18 +45,27 @@ class User(BaseModel):
 
 @app.post("/register")
 def register_user(user: User):
+    """Registrar un usuario y enviarlo a RabbitMQ, además de guardarlo en memoria."""
     print(f"[Users Service] Usuario registrado: {user.dict()}")
+
+    # Save users in memory
+    usuarios_registrados.append(user.dict())
+
+    # Publish messegae in RabbitMQ
     channel.basic_publish(
         exchange="",
         routing_key="notifications",
         body=json.dumps(user.dict())
     )
     print(f"📨 Mensaje enviado a RabbitMQ: {user.email}")
-    return {"message": "Usuario registrado y notificación enviada"}
+    return {"message": "Usuario registrado y notificación enviada", "usuarios": usuarios_registrados}
 
-# Solo para ejecución fuera de Docker (en local)
+@app.get("/usuarios")
+def obtener_usuarios():
+    """Obtener la lista de usuarios registrados temporalmente en memoria."""
+    return {"usuarios": usuarios_registrados}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
